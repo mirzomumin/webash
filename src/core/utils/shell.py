4ffc9097bdbcef94e1e24872c4c1f26f11af5d3a
Mirzomumin <mirzomumin@list.ru>
2025-01-14 01:50:20 +0300

    Add typing to shell functions

--- src/core/utils/shell.py
+++ src/core/utils/shell.py
@@ -1,35 +1,44 @@
 """Module to handle bash commands through subprocess"""
 
 import asyncio
+import os
+from asyncio.subprocess import Process
+from typing import AsyncGenerator
 from uuid import UUID
 
 processes = {}
 
 
-async def create_process(client_id: UUID):
+async def create_process(client_id: UUID) -> Process:
     """Create a reusable subprocess."""
     if client_id in processes:
         return processes[client_id]
+
+    # Dynamically get the home directory
+    home_dir = os.path.expanduser("~")
 
     proc = await asyncio.create_subprocess_shell(
         "/bin/bash",
         stdin=asyncio.subprocess.PIPE,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.STDOUT,
-        cwd="/home/mirzomumin/",
+        cwd=home_dir,
     )
     processes[client_id] = proc
     return proc
 
 
-def delete_process(client_id: UUID) -> None:
+def delete_process(client_id: UUID) -> Process:
     del processes[client_id]
 
 
-async def stream_output(stream, end_marker):
+async def stream_output(
+    stream: asyncio.StreamReader,
+    end_marker: str,
+) -> AsyncGenerator[str]:
     """Stream output from a given stream and yield it"""
     while True:
-        line = await stream.readline()
+        line: bytes = await stream.readline()
         if not line:  # End of the stream
             break
         line_decoded = line.decode().rstrip()
@@ -39,7 +48,11 @@ async def stream_output(stream, end_marker):
     yield "/"
 
 
-async def run_command(*, process, command):
+async def run_command(
+    *,
+    process: Process,
+    command: str,
+) -> AsyncGenerator[str]:
     """Send a command to the process and read its output."""
 
     if not command.strip():

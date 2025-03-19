199f9f7ac6079081e9f7acbd5e19bb66437ff65e
Mirzomumin <mirzomumin@list.ru>
2025-03-19 11:05:23 +0500

    Revert "Merge pull request #159 from mirzomumin/feature/WB-db-configs"
    This reverts commit db74765c26ba612f7b0da18cf7225b5ecc41d1c9, reversing
    changes made to f6b28add0f504c04e396fd5996410f9ae153fad9.

--- src/core/database.py
+++ src/core/database.py
@@ -8,13 +8,7 @@ from sqlalchemy.ext.asyncio import (
 from src.config import settings
 
 
-async_engine = create_async_engine(
-    settings.DB_URL,
-    pool_timeout=30,  # Increase the timeout
-    pool_recycle=1800,  # Recycle connections after 30 minutes
-    pool_size=10,  # Number of connections to keep open
-    max_overflow=5,  # Number of connections to create beyond the pool_size
-)
+async_engine = create_async_engine(settings.DB_URL)
 SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=async_engine)
 
 
